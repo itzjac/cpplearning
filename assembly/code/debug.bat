17ac4c244df5f2aaf073ea717996e040b5b63bed
itzjac <isaacvegan@gmail.com>
2022-08-27 08:37:21 -0500

    Moved full solution to private repo

--- assembly/code/debug.bat
+++
@@ -1,3 +0,0 @@
-@ECHO off
-if NOT EXIST cpuid.sln (devenv /debugexe %~dp0\build\cpuid.exe)
-IF  EXIST cpuid.sln (devenv %~dp0\build\cpuid.sln)
--- assembly/emacs/emacs.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-"C:\Emacs27\x86_64\bin\runemacs.exe" -q -l C:\projects\asm\emacs\.emacs
